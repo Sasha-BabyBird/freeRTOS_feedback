1ae4661e9727b06a5314710a1f99824e413d6df1
Aleksandr Gogotov <44008518+Sasha-BabyBird@users.noreply.github.com>
2022-06-01 06:14:37 +0300

    Update M file
    Add MATLAB computation for verification and readable comments.

--- RTDS2022.m
+++ RTDS2022.m
@@ -1,33 +1,65 @@
-format long
-T = 1; %шаг дискретизации
-num = [1];
+%format long
+
+%model construction
+T = 1; %sample time
+num = 1;
 den = [1 2 2];
-tfsys_c = tf(num, den); %система в TF-форме
-tfsys_d = c2d(tfsys_c, T); %дискретизируем с шагом T
-sssys_d = ss(tfsys_d); %переведём систему в SS-форму
+tfsys_c = tf(num, den); %model in TF form
+tfsys_d = c2d(tfsys_c, T); %discrete model in TF form
+sssys_d = ss(tfsys_d); %discrete model in SS form
 A = sssys_d.A
 B = sssys_d.B
 C = sssys_d.C
 D = sssys_d.D;
-s_desired_observer = [-1 -1.0001]; %собственные значения для наблюдателя
-s_desired_system = [-0.2 + 0.1j -0.2 - 0.1j]; %собственные значения для системы
-z_desired_observer = exp(s_desired_observer*T); %дискретизация собственных значений
+
+%target eigenvalues 
+s_desired_observer = [-1 -1.0001]; 
+s_desired_system = [-0.2 + 0.1j -0.2 - 0.1j]; 
+z_desired_observer = exp(s_desired_observer*T); 
 z_desired_system = exp(s_desired_system*T);
-x0 = [5; -5]; %начальные условия системы
-v0 = [0; 0]; %нулевые начальные значения наблюдателя
+
+%initial conditions and computed matrices
+x0 = [5; -5]; 
+v0 = [0; 0]; 
 K = place(A, B, z_desired_system)
 L_tr = place(A', C', z_desired_observer); 
 L = L_tr'
-Az = A - B*K - L*C %матрица, описывающая динамику наблюдателя
+Az = A - B*K - L*C %matrix which describes the dynamics of the observer
 
+%Simulink modeling result
 sim('OMC_for_RDTS.slx') 
-model_u = model_u'; %результат моделирования в Simulink
+model_u = model_u'; 
 model_y = model_y';
 writematrix(model_u);
 writematrix(model_y);
-rtos_u = readmatrix("model_u_by_RTOS.txt"); %результат расчётов в RTOS
+
+%FreeRTOS computation result
+rtos_u = readmatrix("model_u_by_RTOS.txt"); 
 rtos_y = readmatrix("model_y_by_RTOS.txt");
 
+%MATLAB computation result for verification
+matlab_u = zeros(1, 50);
+matlab_y = zeros(1, 50);
+x = x0;
+z = v0;
+u = 0;
+for i = 1:50
+    x_new = A*x + B*u;
+    y = C*x;
+    x = x_new;
+    matlab_y(i) = y;
+    z_new = Az*z + L*y;
+    u = -K*z;
+    z = z_new;
+    matlab_u(i) = u;
+end
+
+%discrepancy between MATLAB and FreeRTOS computation results
+norm1 = norm(matlab_u(1:49) - rtos_u(2:end));
+norm2 = norm(matlab_y - rtos_y);
+matlab_vs_RTOS_dscr = max(norm1, norm2)
+
+%plotting
 tiledlayout(2, 2);
 
 u_plot = nexttile;
